b65aade25a04930732cd171e42562ee02d4dc07a
Li Yichen <nj_lyc@163.com>
2023-05-14 02:58:33 +0800

    add some fitnesses

--- src/modules/Translator.py
+++ src/modules/Translator.py
@@ -52,7 +52,7 @@ class SingleChromosome_StreamTranslator(Translator):
         self.encode_dim = self.bar_num * round(self.bar_beat * self.beat_duration) * round(1/min_quarter_duration)
         # Upper bound of the elementin encoded array.
         self.max_encode = self.fermata_idx
-        self.obj_arg_info = {'fermata_code': self.max_encode, 'rest_code': 0}
+        self.obj_arg_info = {'fermata_code': self.max_encode, 'rest_code': 0, 'translator': self}
 
     def encode(self, stream: Stream, *args: Any, **kwds: Any) -> np.ndarray:
         stream_arr = np.zeros(self.length, dtype=np.int8)
@@ -118,7 +118,7 @@ class PitchOnlyTranslator(Translator):
         self.encode_dim = self.bar_num * round(self.bar_beat * self.beat_duration) * round(1/quarter_duration)
         # Upper bound of the elementin encoded array.
         self.max_encode = len(self.pitch2idx)
-        self.obj_arg_info = {'rest_code': 0}
+        self.obj_arg_info = {'rest_code': 0, 'translator': self}
         
     def decode(self, stream_arr: np.ndarray) -> Stream:
         ret_stream = Stream()
--- src/modules/obj/SingleFunc.py
+++ src/modules/obj/SingleFunc.py
@@ -1,6 +1,9 @@
 import numpy as np
 from typing import Literal
 from src.modules.obj.obj_wrapper import OBJ_SGL_FUNCS, OBJ_SGL_OPT_TARGET
+from src.utils.utils import _get_available_pitches
+from src.modules.Translator import Translator
+
 
 def register(name: str, min_max: Literal[1, -1]):
     def wrapper(func):
@@ -10,11 +13,11 @@ def register(name: str, min_max: Literal[1, -1]):
     return wrapper
     
 @register("naive", 1)
-def naive_fitness(x: np.ndarray, rest_code: int, fermata_code: int):
+def naive_fitness(x: np.ndarray, rest_code: int, fermata_code: int, translator: Translator):
     return np.sum(x)
 
 @register("continuous", -1)
-def continuous_fitness(x: np.ndarray, rest_code: int, fermata_code: int):
+def continuous_fitness(x: np.ndarray, rest_code: int, fermata_code: int, translator: Translator):
     rest_cnt = 0
     farmata_cnt = 0
     note_cnt = 0
@@ -28,5 +31,55 @@ def continuous_fitness(x: np.ndarray, rest_code: int, fermata_code: int):
     return farmata_cnt / len(x)
 
 @register("continuous_pitch_only", -1)
-def continuous_fitness(x: np.ndarray, rest_code: int):
+def continuous_fitness(x: np.ndarray, rest_code: int, translator: Translator):
     return np.var(x)
+
+@register("random", 1)
+def random_fitness(x: np.ndarray, rest_code: int, translator: Translator):
+    return np.random.random()
+
+@register("no_octave_jump", -1)
+def cn_g1_fitness(x: np.ndarray, rest_code: int, translator: Translator):
+    x = x[x != rest_code]
+    return np.mean(np.abs(np.diff(x)) <= 12)
+
+@register("only_CDEGA", -1)
+def cn_g4_fitness(x: np.ndarray, rest_code: int, translator: Translator):
+    pitches = ["G3", "A3", "C4", "D4", "E4", "G4", "A4", "C5", "D5", "E5",]
+    good_idx = [translator.pitch2idx[pitch] for pitch in pitches]
+    return np.bitwise_or.reduce([ (x == idx) for idx in good_idx ]).mean()
+
+@register("not_monotone", -1)
+def cn_g5_fitness(x: np.ndarray, rest_code: int, translator: Translator):
+    x = x[x != rest_code]
+    return np.abs(x[-1] - x[0]) <= len(x)
+
+@register("not_flat", -1)
+def cn_g6_fitness(x: np.ndarray, rest_code: int, translator: Translator):
+    x = x[x != rest_code]
+    return (np.diff(x) != 0).mean()
+
+@register("not_intense", 1)
+def cn_g7_fitness(x: np.ndarray, rest_code: int, translator: Translator):
+    x = x[x != rest_code]
+    return np.var(np.diff(x))
+
+@register("l1_loss", 1)
+def l1_fitness(x: np.ndarray, rest_code: int, translator: Translator):
+    x = x[x != rest_code]
+    return np.abs(np.diff(x)).mean()
+
+@register("reduce_rest", -1)
+def reduce_rest(x: np.ndarray, rest_code: int, translator: Translator):
+    return np.mean(x != rest_code)
+
+
+@register("weighted", -1)
+def weighted_fitness(x: np.ndarray, rest_code: int, translator: Translator):
+    return \
+          cn_g4_fitness(x, rest_code, translator) \
+        - cn_g7_fitness(x, rest_code, translator) * 0.001 \
+        + cn_g6_fitness(x, rest_code, translator) * 0.6 \
+        - reduce_rest(x, rest_code, translator) * 0.5 \
+        + l1_fitness(x, rest_code, translator) * 0.007 \
+
